f11c0140e9ca32f1037b7060fbbdad48a31b8078
zelvinside <zelvinside@gmail.com>
2016-09-05 17:37:35 +0800

    New: 	1. add new header layout (header-login.php) + responsive 	2. add new page (userpage.php) + responsive
    Bug fixing:
    	1. change order "scrollToFixed" function

---
+++ addons/default/themes/coketune/styling/header-login.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="">
+
+<head>
+	<meta charset="utf-8">
+	<title> Coca Cola </title>
+	<!--Let browser know website is optimized for mobile-->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<meta name="robots" content="" />
+	
+	<!-- Favicon -->
+	<link rel="apple-touch-icon-precomposed" sizes="57x57" href="img/lib/favicon/apple-touch-icon-57x57.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/lib/favicon/apple-touch-icon-114x114.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/lib/favicon/apple-touch-icon-72x72.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="img/lib/favicon/apple-touch-icon-144x144.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="60x60" href="img/lib/favicon/apple-touch-icon-60x60.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="120x120" href="img/lib/favicon/apple-touch-icon-120x120.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="76x76" href="img/lib/favicon/apple-touch-icon-76x76.png" />
+	<link rel="apple-touch-icon-precomposed" sizes="152x152" href="img/lib/favicon/apple-touch-icon-152x152.png" />
+	<link rel="icon" type="image/png" href="img/lib/favicon/favicon-196x196.png" sizes="196x196" />
+	<link rel="icon" type="image/png" href="img/lib/favicon/favicon-96x96.png" sizes="96x96" />
+	<link rel="icon" type="image/png" href="img/lib/favicon/favicon-32x32.png" sizes="32x32" />
+	<link rel="icon" type="image/png" href="img/lib/favicon/favicon-16x16.png" sizes="16x16" />
+	<link rel="icon" type="image/png" href="img/lib/favicon/favicon-128.png" sizes="128x128" />
+	<meta name="application-name" content="&nbsp;"/>
+	<meta name="msapplication-TileColor" content="#FFFFFF" />
+	<meta name="msapplication-TileImage" content="img/lib/favicon/mstile-144x144.png" />
+	<meta name="msapplication-square70x70logo" content="img/lib/favicon/mstile-70x70.png" />
+	<meta name="msapplication-square150x150logo" content="img/lib/favicon/mstile-150x150.png" />
+	<meta name="msapplication-wide310x150logo" content="img/lib/favicon/mstile-310x150.png" />
+	<meta name="msapplication-square310x310logo" content="img/lib/favicon/mstile-310x310.png" />
+	<!-- end Favicon -->
+	
+	<link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+
+	<header>
+		<div id="header-inner" class="container">
+			<div class="row">
+				<div id="logo">
+					<a href="#"><img src="img/coke/logo-red.png"	 /></a>
+				</div> <!-- #logo -->
+				<a href="javascript:void(0)" id="toggle-nav">toggle navigation</a>
+				
+				<!-- NAVIGATION WIDE SCREEN -->
+				<nav id="top">
+					<ul>
+						<li><a href="#">cara mengikuti kompetisi</a></li>
+						<li>
+							<a href="#">
+								<div class="row">
+									<div class="userProfile-image">
+										<img src="img/coke/demo-user-profile-picture.jpg"/>
+									</div> <!-- .image -->
+									<div class="userProfile-info">
+										<div class="name text-link">Tatjana saphira</div>
+									</div> <!-- .userProfile-info -->
+								</div> <!-- .row -->
+							</a>
+						</li>
+						<li><a href="#">keluar</a></li>
+					</ul>
+				</nav> <!-- #top -->
+				<!-- END NAVIGATION WIDE SCREEN -->				
+			</div> <!-- .row -->
+			
+			<!-- NAVIGATION MOBILE -->
+			<nav id="mobile">
+				<ul>
+					<li>
+						<div class="row profile">
+							<div class="userProfile-image">
+								<img src="img/coke/demo-user-profile-picture.jpg"/>
+							</div> <!-- .image -->
+							<div class="userProfile-info">
+								<div class="name text-link">Tatjana saphira</div>
+								<a href="#" class="link">keluar</a>
+							</div> <!-- .userProfile-info -->
+						</div> <!-- .row -->
+					</li>
+					<li><a href="#">beranda</a></li>
+					<li><a href="#">cara mengikuti kompetisi</a></li>
+					<li><a href="#">syarat dan ketentuan</a></li>
+					<li><a href="#">kebijakan privasi</a></li>
+				</ul>
+			</nav> <!-- #mobile -->
+			<!-- END NAVIGATION MOBILE -->			
+			
+		</div> <!-- #header-inner -->
+	</header>
