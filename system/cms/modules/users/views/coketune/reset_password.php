89949cefbb821e0bc9c53b9e081d4191e84f264c
natanael <natanael@natanael-desktop>
2016-09-06 14:38:20 +0800

    update

--- system/cms/modules/users/views/coketune/reset_password.php
+++ system/cms/modules/users/views/coketune/reset_password.php
@@ -1,7 +1,27 @@
-
-<?php echo form_error('email');?>
-
-<?php echo form_open()?>
-<?php echo form_input('email', '')?>
-<?php echo form_submit('f_lanjut', 'LANJUT')?>
-<?php echo form_close()?>
+<main>
+	<section id="register" class="date-check fullScreen">
+		<div id="background-img" class="fluid-img register">
+			<div id="register-inner" class="container">
+				<div class="row">
+					<div id="figure" class="wide panel">lupa kata sandi</div>
+					<div class="panel" id="email-register">
+						<div class="title">
+							<h4>masukan email kamu</h4>
+						</div> <!-- .title -->
+						<?php echo cmc_form_open('user-register','');?>	
+						<form id="user-register">
+							<div class="column">
+								<input id="email" type="email" placeholder="alamat email mu">
+							</div> <!-- .column -->
+							<div class="button-action-wrapper">
+								<?php echo form_submit('f_lanjut', 'LANJUT','class="button rounded border primary"')?>
+							
+							</div> <!-- .button-action-wrapper -->
+						<?php echo form_close()?> <!-- #user-register -->
+					</div> <!-- .panel -->
+				</div> <!-- .row -->
+			</div> <!-- #register-inner -->
+		</div> <!-- #background-img -->
+	</section> <!-- #login -->
+	
+</main>
