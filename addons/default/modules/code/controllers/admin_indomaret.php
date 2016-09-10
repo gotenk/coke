ce54d5125360f3c49e83bd8f0e49cb6677c09a92
Robertus <rrahardj@gmail.com>
2016-09-10 17:53:23 +0800

    no message

--- addons/default/modules/code/controllers/admin_indomaret.php
+++ addons/default/modules/code/controllers/admin_indomaret.php
@@ -37,7 +37,7 @@ class Admin_indomaret extends Admin_Controller
 
         $data = array();
         $per_page = Settings::get('records_per_page');
-        $total_rows = 10; //->code_m->getIndomaretCodeList($parameter)->num_rows();
+        $total_rows = $this->code_m->getIndomaretCodeList($parameter)->num_rows(); // Ini bermasalah ya
         $pagination = create_pagination(ADMIN_URL.'/code/indomaret/index', $total_rows, $per_page, 5);
         $codes = $this->code_m->getIndomaretCodeList($parameter, $pagination)->result();
 
